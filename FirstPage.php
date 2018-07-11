671e5b51a568a14d242c7095fa02847f3bef8089
Sweta <swetaiitm17@gmail.com>
2018-07-11 23:52:56 +0530

    Add files via upload

---
+++ FirstPage.php
@@ -0,0 +1,142 @@
+<html>
+
+<head>
+
+<style>
+a.exe1:link, a.exe1:visited {
+   
+    color:  white;
+    padding: 14px 20px;
+    text-align: center; 
+    text-decoration: none;
+    transition: 0.5s;
+    padding-bottom: 10px;
+    display:inline-block;    
+}
+
+a.exe1 {
+  
+   background: linear-gradient(0deg, currentcolor, currentcolor) bottom center no-repeat;
+  background-size: 0px 3px;
+}
+a.exe1:hover , a.exe1:active {  
+	text-decoration:none;border-bottom-width: 0 0 1px;background-size: 70% 2px;white-space: wrap;
+}
+span{
+padding-left: 100px;
+}
+
+
+a.tst:link, a.tst:visited {
+   
+    color:  white;
+    padding: 0px 0px;
+    text-align: center; 
+    text-decoration: none;
+    display: inline;
+    transition: 0s;
+}
+a.tst:hover, a.tst:active{
+
+    text-decoration:underline;
+}
+
+
+img.exe2 {
+     border-radius: 50%;
+    opacity: 0.8;
+    filter: alpha(opacity=50);
+}
+
+img.exe2:hover {
+    opacity: 1.0;
+    filter: alpha(opacity=100);
+}
+
+
+/* The typing effect */
+@keyframes typing {
+  from { width: 0 }
+  to { width: 100% }
+}
+
+/* The typewriter cursor effect */
+@keyframes blink-caret {
+  from, to { border-color: transparent }
+  50% { border-color: orange; }
+}
+
+</style>
+
+</head>
+
+<body background="background.jpg">
+
+<div style="height:100%;width:100%"> 
+        <div  style="float:top; display:inline-block;  height:10%;width:100%; font-family:Arial; font-size:18px;padding-top: 15px; padding-left: 130px;" class="div1">
+		<a class="exe1" href="FirstPage.php" style="color: white; font-weight:bold ; " >Sweta02018</a>
+		<span><a class="exe1" href="FirstPage.php" style="color: white;">Home</a></span>
+		<a class="exe1" href="Publications.php" style="color: white;">Publications</a>
+		<a class="exe1" href="Projects.php" style="color: white;">Projects</a>
+		<a class="exe1" href="Blogs.php" style="color: white;">Blog Posts</a>
+		<a class="exe1" href="files/SWETA _CV_TMU_final.pdf" style="color: white;" target="_blank">Resume</a>
+	</div> 
+
+	
+	
+        <div style="float:bottum; display:inline-block;  height:90%;width:100%; padding-top:30px;" class="div2">
+		
+		<div style="float:left; display:inline-block;  height:100%;width:10%;" class="div2">
+		
+		</div>
+		
+		<div  style="float:left; font-family:Arial; color: white; display:inline;  height:100%;width:15%;text-align:left;font-size:12" align="center"  class="div2">
+			<img src="sweta.jpg" alt="Card Back" style="width:160px; height: 130px;" class="exe2">
+			<p style="font-family:Arial;font-size:17;font-weight:bold;">Sweta Kumari<p>
+			<img src="a.jpg" alt="Avatar" style="width:9px;height:12px">
+			<a class="tst" href="https://www.google.co.in/maps/place/IIT+MADRAS/@13.0052137,80.2398125,17z/data=!3m1!4b1!4m5!3m4!1s0x3a5267f29aa9a61f:0x24ef264085e6a094!8m2!3d13.0052137!4d80.2420012" style=" pading:0px 0px;display:inline;color: white;"> 
+			IIT Madras, India</a>
+			</br></br>
+			<img src="emaill.png" alt="Avatar" style="width:12px;height:8px">
+			<a class="tst" href="mailto:swetaiitm17@gmail.com" style=" padding:0px 0px;display:inline;color: white;" >
+			Email</a>
+			</br></br>
+			<img src="github.png" alt="Avatar" style="width:13px;height:11px;">
+			<a class="tst" href="https://github.com/sweta02018" style=" padding:0px 0px;display:inline;color: white;">
+			Github
+			</a>
+			
+			
+		</div>
+		<!--
+ 		<div style="float:left; display:inline-block;  height:100%;width:2%;" class="div2">
+			<hr width="1" size="1000">
+		</div>
+		-->
+		<div style="float:left; font-family:Arial;display:inline-block;  height:100%;width:75%;" class="div3">
+			<h2 class="typewriter" style="color: white;border-bottom: 2px solid  white;width:90%">About me!</h2>
+			<p style="color: white; padding:5px; line-height: 180%; margin-top:5px; margin-bottom:20px;  margin-left:55px;  margin-right:200px;  ">
+			I am a 2nd-year Ph.D scholar in Computational Neuroscience Laboratory at department of Biotechnology, Indian Institute of Technology
+			, Madras. I am working under the supervision of Prof. Srinivasa Chakravarthy.
+			</p>
+			
+
+			<h2 class="typewriter" style="color: white;border-bottom: 2px solid  white;width:90%;margin-top:350px;">Find me!</h2>
+			
+			<div class="icon-bar" style=" padding:8px; line-height: 180%; margin:auto;" align="center">
+			<a href="mailto:swetaiitm17@gmail.com" class="google" style=" padding-right:8px;"><img src="gmail1.png" alt="Avatar" style="width:45px;height:30px"></a>
+			<a href="https://www.linkedin.com/in/sweta-kumari-b7a9a5117/" class="linkedin"><img src="linkedin2.png" alt="Avatar" style="width:40px;height:40px"></i></a>
+			<a href="https://twitter.com/Sweta96233?s=03" class="twitter"><img src="twitter (1).png" alt="Avatar" style="width:50px;height:50px"></a> 
+			<a href="https://github.com/sweta02018" class="github"><img src="Octocat.png" alt="Avatar" style="width:50px;height:50px"></a> 
+			<a href="https://www.kaggle.com/sweta12" class="kaggel"><img src="Kaggle.png" alt="Avatar" style="width:50px;height:50px"></a>
+  			  
+  			<!--<a href="#" class="youtube" ><i class="fa fa-youtube"></i></a> -->
+			</div>
+		</div> 
+	</div>
+	
+</div> 
+
+</body>
+
+</html>
